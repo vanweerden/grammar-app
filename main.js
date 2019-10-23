083414c50fc1b69492c0c94f6b1ed4c504ddea01
vanweerden <ando.vanweerden@gmail.com>
2019-10-24 07:04:59 +0800

    Added content, layout, and basic styles

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-var correct = ["have been learning", "'ve been learning"];
+// TO SOLVE
+// grab input from each unique question
+// listen for Enter being pushed OR button pushed
+// access answer from array
+// output check or x to unique question
+// display base of verb to use
+
+
+const answers = [
+  ["have been learning", "'ve been learning"],
+  ["has never seen", "'s never seen"],
+  ["has read", "'s read"],
+  ["have known", "'ve known"],
+  ["has been crying", "'s been crying"],
+  ["have had", "'ve had"],
+  ["have seen", "'ve seen"],
+  ["has been washed", "'s been washed"],
+];
 
 function checkAnswer(ans) {
-  if (correct.includes(ans)) {
+  if (correct.includes(answers[number])) {
     tickBox();
   } else {
     exBox();
@@ -9,9 +26,11 @@ function checkAnswer(ans) {
 }
 
 function tickBox() {
-  document.getElementById("checkbox").innerHTML = "[✓]";
+  document.getElementsByClassName("checkbox").innerHTML = "[✓]";
+  document.getElementsByClassName("checkbox").style.color = "#2776b3";
 }
 
 function exBox() {
-  document.getElementById("checkbox").innerHTML = "[x]";
+  document.getElementsByClassName("checkbox").innerHTML = "[x]";
+  document.getElementsByClassName("checkbox").style.color = "#FF662A";
 }
