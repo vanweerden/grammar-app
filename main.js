15e4e5a229a1996285876e5c6f77bba877566b7f
vanweerden <ando.vanweerden@gmail.com>
2019-10-25 06:47:33 +0800

    Use JS to add unique ID to each sentence

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // TO SOLVE
-// grab input from each unique question
-// listen for Enter being pushed OR button pushed
+// add form to each question
+// grab input from each unique question using event listener for 'submit'
+    // hitting enter or button
 // access answer from array
 // output check or x to unique question
 // display base of verb to use
+  //display as placeholder text? add base verb to answers array--make into object?
 
-
+// Sub arr index matches input-number ID and checkbox ID
 const answers = [
   ["have been learning", "'ve been learning"],
   ["has never seen", "'s never seen"],
@@ -17,12 +19,37 @@ const answers = [
   ["has been washed", "'s been washed"],
 ];
 
-function checkAnswer(ans) {
-  if (correct.includes(answers[number])) {
-    tickBox();
-  } else {
-    exBox();
+// Grabs all answer input fields
+const clozeInput = [...document.querySelectorAll('.attempt')];
+
+// Gives each input element a unique ID which matches answer subarray
+(function numberInput() {
+  for (let i = 0; i < clozeInput.length; i++) {
+    clozeInput[i].setAttribute("data-sentence", i);
   }
+})();
+
+// Does the same to the checkboxes
+(function numberCheckboxes() {
+  const checkboxes = [...document.querySelectorAll('.checkbox')];
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].setAttribute("id", "checkbox-" + i);
+  }
+})();
+
+// Listen for input from user (need to loop through all elements)
+(function inputHandler(e) {
+  clozeInput.addEventListener('input', checkAnswer());
+})();
+
+function checkAnswer(ans) {
+//   // Grab data-sentence value for sentence number
+//   // if (answers[sentence number].includes(ans)) {
+//   //   tickBox();
+//   // } else {
+//   //   exBox();
+//   // }
 }
 
 function tickBox() {
